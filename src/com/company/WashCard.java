03f776a0929b9809f0536fc936b4bcba04adcd7f
AndreasGargulak <andr6230@stud.kea.dk>
2020-10-07 12:25:13 +0200

    constructor og getters i WashCard class

--- src/com/company/WashCard.java
+++ src/com/company/WashCard.java
@@ -4,7 +4,16 @@ public class WashCard {
     private int balance;
     private String cardNumber;
 
-    public WashCard(){
+    public WashCard(int balance, String cardNumber){
+        this.balance=balance;
+        this.cardNumber=cardNumber;
+    }
 
+    public int getBalance(){
+        return balance;
+    }
+
+    public String getCardNumber(){
+        return cardNumber;
     }
 }
