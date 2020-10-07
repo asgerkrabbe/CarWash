14c7bf4559c0a7770963d78aac261c0b5e4c562b
AndreasGargulak <andr6230@stud.kea.dk>
2020-10-07 12:59:21 +0200

    constructor og getters i WashCard class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,25 @@
 package com.company;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 	// write your code here
+        String file = "/Users/AndreasGargulak/Documents/KEA/CarWash/validNumbers";
+        File washCardId = new File(file);
+        Scanner inputUser = new Scanner(System.in);
+        Scanner inputWashCardId = new Scanner(file);
 
+        System.out.println("Insert your card(Enter cardnumber) to start carwash: ");
+        String enteredNumber = inputUser.nextLine();
+
+        while (inputWashCardId.hasNext()){
+            inputWashCardId.equals(enteredNumber);
+
+
+        }
     }
 }
